a6307df9616a2592e1e9237dd4b4e2254449719e
CambraBusinessAnalytics <cambraconsultoria@gmail.com>
2025-05-12 22:14:40 -0400

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 from dash import html, dcc, page_container
 
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-#server = app.server
+server = app.server
 app.layout = html.Div([
     # Encabezado principal con fondo sutil
     html.Header(
